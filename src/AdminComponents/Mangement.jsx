a85de9072d4b4e2b6afe4d35dda1249a0431f9c2
Saadafza <133769979+Saadafza@users.noreply.github.com>
2023-10-05 23:46:48 +0500

    MANAGER

--- src/AdminComponents/Mangement.jsx
+++ src/AdminComponents/Mangement.jsx
@@ -1,8 +1,20 @@
 function Mangement(){
     return(
         <>
-        
-        </>
-    )
-}
+       
+          <div className="main-container">
+            <h3 className="ever mt-2">EverGreen Solutions</h3>
+            <div className="admin">
+              <p className="text-danger">System Admin</p>
+              <h5 style={{ lineHeight: "0.4",textAlign:"center" }}>Logout</h5>
+            </div>
+          </div>
+          <div className="conatiner create-account">
+            <button>Create System Account </button>
+            <button >Create Manager Account </button>
+          </div>
+      
+      </>
+    );
+  }
 export default Mangement;
